27213901e26785a69c2b952efe7b3c832638d22c
Luis Rodriguez <lrodriguez@konradgroup.com>
2018-11-16 14:48:50 -0600

    ES list and prettier added

--- src/App.js
+++ src/App.js
@@ -2,40 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-import Todo from "./pages/todo"
+import Todo from './pages/todo';
 
 class App extends Component {
-state={
-  value:'',
-}
+  state = {
+    value: '',
+  };
 
-onChangeInput= e => {
-  this.setState({
-    value:e.target.value,
-
-  });
-};
+  onChangeInput = () => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
 
   render() {
     return (
-      <div className="App">
-
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Todo id="inputTest" value={this.state.value} onChange={this.onChangeInput} />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="row">
+          Test
+          <Todo onChange={this.onChangeInput} value={this.state.value} />
+        </div>
       </div>
     );
   }
